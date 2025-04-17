c24acf282e0767c1886aed2d9f42519a9f121190
Ice-San <rotommine@gmail.com>
2025-04-17 21:35:58 +0100

    BAZING-2 | Front-End | Fixing intro animation bugs and adding favicon!

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,5 +1,7 @@
 const intro = document.querySelector(".intro");
+const hero = document.querySelector(".hero");
 
 setTimeout(() => {
+    hero.classList.remove("invisible");
     intro.classList.add("invisible");
 }, 8405);
