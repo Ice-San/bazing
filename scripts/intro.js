ddf6d44900340367d5936d45986c2692f62d6ac2
Ruben Costa <rubencosta.dev.2004@gmail.com>
2025-05-02 19:34:50 +0100

    BAZING-5 | Front-End | Fixing some bugs on hero and trending books sections!

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,10 +1,12 @@
 const intro = document.querySelector(".intro");
 const content = document.querySelector(".content");
+const hero = document.querySelector(".hero");
 
 setTimeout(() => {
     content.classList.remove("invisible");
-}, 8200);
 
-setTimeout(() => {
+    hero.classList.remove("layer-back");
+    hero.classList.add("layer-front");
+    
     intro.classList.add("invisible");
 }, 8405);
