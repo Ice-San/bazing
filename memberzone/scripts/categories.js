fb0654c979df97583e31868d04610f66d7f80190
Ruben Costa <rubencosta.dev.2004@gmail.com>
2025-05-27 01:21:14 +0100

    BAZING-10 | Front-End | Created Header & Categories on MemberZone Page!

---
+++ memberzone/scripts/categories.js
@@ -0,0 +1,44 @@
+const container = document.querySelector(".categories-content");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener('mousedown', (e) => {
+  isDown = true;
+
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('mouseleave', () => isDown = false );
+container.addEventListener('mouseup', () => isDown = false );
+
+container.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+
+  e.preventDefault();
+
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 1.5;
+
+  container.scrollLeft = scrollLeft - walk;
+});
+
+container.addEventListener('touchstart', (e) => {
+  isDown = true;
+
+  startX = e.touches[0].pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('touchend', () => isDown = false );
+
+container.addEventListener('touchmove', (e) => {
+  if (!isDown) return;
+
+  const x = e.touches[0].pageX - container.offsetLeft;
+  const walk = (x - startX) * 1.5;
+
+  container.scrollLeft = scrollLeft - walk;
+});
